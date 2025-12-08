f04ee3305e815704034fb338b9febfc2e1d879e2
vishnu <iam.vishnu.bhaskar@gmail.com>
2025-12-08 12:19:41 +0530

    Add Email Sign-in page and Auth layout; update global styles and home page

---
+++ app/auth/email-signin/page.tsx
@@ -0,0 +1,24 @@
+import AuthLayout from '@/layouts/auth'
+import Link from 'next/link'
+
+const EmailSigninPage = async () => {
+    return (
+        <AuthLayout
+            heading={
+                <>Sign In to Your <span className='text-theme-primary'>Workspace</span></>
+            }
+            content='Manage orders, track inventory, process fulfillment, support customers, and streamline store operations from one platform.'
+            afterContent={
+                <p>
+                    Forgot password?
+                    <Link
+                        href={'/auth/forget-password'}
+                        className='text-action-button font-semibold'
+                    >&nbsp;Reset</Link>
+                </p>
+            }
+        ></AuthLayout>
+    )
+}
+
+export default EmailSigninPage
---
+++ layouts/auth/index.tsx
@@ -0,0 +1,37 @@
+import { PropsWithChildren, ReactNode } from 'react'
+
+const AuthLayout = async ({
+    children,
+    content,
+    heading,
+    afterContent,
+}: PropsWithChildren<{
+    heading: ReactNode | string,
+    content: string,
+    afterContent: ReactNode,
+}>) => {
+    return (
+        <div
+            className='min-h-dvh w-full flex items-stretch'
+        >
+            <div
+                className='w-full'
+            >Auth Layout</div>
+            <div
+                className='w-full bg-theme-primary/10'
+            >
+                <div
+                    className='max-w-[400px] mx-auto min-h-full flex flex-col justify-center gap-5'
+                >
+                    <h1
+                        className='text-5xl font-bold leading-[62px]'
+                    >{heading}</h1>
+                    <p>{content}</p>
+                    {afterContent}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default AuthLayout
