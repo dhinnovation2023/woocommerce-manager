62ca051ed95f36f2ba589a9a9364c8ef76b912d2
vishnu <iam.vishnu.bhaskar@gmail.com>
2025-12-12 09:14:21 +0530

    url.parse node version issue fix: try 1;

--- woocommerce/orders/get-all.ts
+++ woocommerce/orders/get-all.ts
@@ -10,21 +10,27 @@ export interface GetAllWcOrderRequestData {
 export async function getAllWcOrders(options: GetAllWcOrderRequestData) {
     return new Promise<WcOrdersInterface[]>(async (resolve, reject) => {
         try {
+            console.log("Debug 1")
             const woocommerceApi = await getWoocommerceApi();
-
+            console.log("Debug 2")
+            
             const findQuery: {
                 [key: string]: string,
             } = {};
-
+            console.log("Debug 3")
+            
             for (const [key, value] of Object.entries(options)) {
                 if (value) {
                     findQuery[key] = value;
                 }
             }
-
+            console.log("Debug 4")
+            
             const response = await woocommerceApi.get<WcOrdersInterface[]>('/orders', {
                 params: findQuery,
             })
+
+            console.log("Debug 5")
 
             return resolve(response.data);
         } catch (err) {
